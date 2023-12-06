40adc408499c9131dfc89471c2f1b4fc24c35438
Jasel <jllopfan2@yahoo.es>
2023-12-06 14:58:54 +0100

    Initial commit

---
+++ src/main/java/net/apocalypse/origins/Apocalypse_Origins_Client.java
@@ -0,0 +1,10 @@
+package net.apocalypse.origins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Apocalypse_Origins_Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
